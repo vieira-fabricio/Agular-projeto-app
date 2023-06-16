b5497a884d0f855c0c5c3e26e4715fe82ce3cf1b
Fabricio Vieira <bvieira.fabricio@gmail.com>
2023-06-16 20:35:16 -0300

    implementação dos primeiros componentes angular

---
+++ bankline-app/src/app/components/movimentacao-list/movimentacao-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-movimentacao-list',
+  templateUrl: './movimentacao-list.component.html',
+  styleUrls: ['./movimentacao-list.component.css']
+})
+export class MovimentacaoListComponent implements OnInit {
+  
+  movimentacoes:any;
+   
+  constructor(){
+    
+  }
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+  
+
+  
+}
