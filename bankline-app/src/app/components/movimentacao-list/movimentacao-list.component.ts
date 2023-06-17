622e40d3a923ef52b9333ee30671aa553b71ccdb
Fabricio Vieira <bvieira.fabricio@gmail.com>
2023-06-17 19:45:54 -0300

    implementação dos serviços

--- bankline-app/src/app/components/movimentacao-list/movimentacao-list.component.ts
+++ bankline-app/src/app/components/movimentacao-list/movimentacao-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MovimentacaoService } from 'src/app/services/movimentacao.service';
 
 @Component({
   selector: 'app-movimentacao-list',
@@ -9,12 +10,24 @@ export class MovimentacaoListComponent implements OnInit {
   
   movimentacoes:any;
    
-  constructor(){
+  constructor(private movimentacaoService: MovimentacaoService){
     
   }
 
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
+    this.listMovimentacoes();
+  }
+
+  listMovimentacoes(): void {
+    this.movimentacaoService.list()
+        .subscribe(
+          data => {
+            this.movimentacoes = data;
+            console.log(data);
+          },
+          error => {
+            console.log(error);
+          })
   }
   
 
