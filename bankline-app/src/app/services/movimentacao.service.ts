d747a0f349b836ce463319d2864a1b0eaea56ac1
Fabricio Vieira <bvieira.fabricio@gmail.com>
2023-06-21 22:17:25 -0300

    aprimoramento da tela  de exibição das movimentações
    criado o filtro para buscar as movimentações por correntista

--- bankline-app/src/app/services/movimentacao.service.ts
+++ bankline-app/src/app/services/movimentacao.service.ts
@@ -15,6 +15,10 @@ export class MovimentacaoService {
     return this.http.get(`${baseUrl}/movimentacoes`);
   }
 
+  findByIdConta(idConta: any): Observable<any> {
+    return this.http.get(`${baseUrl}/movimentacoes/${idConta}`);
+  }
+
   create(movimentacao: any): Observable<any> {
     return this.http.post(`${baseUrl}/movimentacoes`, movimentacao);
   }
